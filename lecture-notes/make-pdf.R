7282937fec2e6532595eb8854007e5c2443ea6c2
Ivan Rudik <irudik@cornell.edu>
2023-01-22 17:38:30 -0500

    add pdf

---
+++ lecture-notes/make-pdf.R
@@ -0,0 +1,9 @@
+# Makes pdf slides for all lectures
+lapply(list.files("lecture-notes", pattern = "*.html", recursive = T, full.names = TRUE)[1], 
+       function(file) {
+         xaringan::decktape(
+           file, 
+           output = paste0(tools::file_path_sans_ext(file), ".pdf")
+         )
+       }
+)
