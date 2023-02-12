41a92aaed3335c53cb92a677b50a6241ff8b9648
Ivan Rudik <irudik@cornell.edu>
2023-02-12 15:55:14 -0500

    update optim slides

--- lecture-notes/make-pdf.R
+++ lecture-notes/make-pdf.R
@@ -1,5 +1,5 @@
 # Makes pdf slides for all lectures
-lapply(list.files("lecture-notes", pattern = "*.html", recursive = T, full.names = TRUE)[3], 
+lapply(list.files("lecture-notes", pattern = "*.html", recursive = T, full.names = TRUE)[4], 
        function(file) {
          xaringan::decktape(
            file, 
